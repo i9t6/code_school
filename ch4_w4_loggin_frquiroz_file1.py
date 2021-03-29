df3f9d82e16c600a3c39bd4cacebe6b97fe318a8
Francisco Quiroz <frquiroz@hotmail.com>
2021-03-29 09:35:05 -0600

    ch4_files_finales

--- ch4_w4_loggin_frquiroz_file1.py
+++ ch4_w4_loggin_frquiroz_file1.py
@@ -2,8 +2,6 @@
 
 from netmiko import ConnectHandler as cn
 from datetime import datetime as dt
-import logging
-
 
 devices = [
     {'device_type' : 'cisco_ios',
@@ -26,41 +24,19 @@ devices = [
 shows = ['clock','int description','ip interface brief','ip route','inventory','run']
 date = dt.now().date().strftime('%Y-%d-%m')
 
-
-console_formartter = logging.Formatter('%(asctime)s-::-%(levelname)s-::-%(message)s')
-logfile_formartter = logging.Formatter('%(levelname)s~%(asctime)s~message:%(message)s~module:%(module)s')
-
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.WARNING)
-console_handler.setFormatter(console_formartter)
-
-logfile_handler = logging.FileHandler(f"my_logs_{date}.log",mode ="w")
-logfile_handler.setLevel(logging.DEBUG)
-logfile_handler.setFormatter(logfile_formartter)
-
-my_logger = logging.getLogger()
-my_logger.addHandler(console_handler)
-my_logger.addHandler(logfile_handler)
-my_logger.setLevel(logging.DEBUG)
-
 for d in devices:
     hostname = d['host']
     try: 
-        my_logger.warning(f"Initiate connection: {hostname}")
         net_connect = cn(**d)
     except:
-        my_logger.warning(f"Failed Connection -------> {hostname}")
+        print(f'FAILED CONNECTION ----> {hostname}')
     else:
-        my_logger.warning(f"Success")
         f = open(f'{hostname}_{date}.txt','w')
-        my_logger.warning(f"Create a file {hostname}_{date}.txt")
-        my_logger.warning(f"Inititate sending commands")
         f.write(f'\nDevice: {hostname}')
         for c in shows:
             output = net_connect.send_command("show "+c)
             f.write(f'\n\n=========================== show {c}: ===========================\n\n')
-            #f.write(f' show {c} '.center(20, '='))
+            
             f.write(output)
-            my_logger.warning(f"Show {c} --> OK")
         f.close()
-    my_logger.warning(f"Close connection:{hostname}")
+        print(f'SUCCESS ----------> {hostname}')
---
+++ teoria _logging.py
@@ -0,0 +1,30 @@
+#!/home/paco/py3/bin/python
+import logging
+
+my_logger = logging.getLogger()
+#my_handler = logging.FileHandler("temp.log")
+my_handler = logging.StreamHandler()
+my_logger.addHandler(my_handler)
+#logging.basicConfig(level=logging.DEBUG)
+my_logger.setLevel(logging.DEBUG)
+a = 1
+b = 2
+c = a + b
+
+texto = "algo muy largo para nada"
+
+my_logger.debug(f"debug message {a}")
+my_logger.info(f"info {b}")
+my_logger.warning(f"warning {c}")
+my_logger.error(f"error {c+1}")
+my_logger.critical(f"Critical {c+2} ")
+
+
+"""
+logging.debug(f"debug message {a}")
+logging.info(f"info {b}")
+logging.warning(f"warning {c}")
+logging.error(f"error {c+1}")
+logging.critical(f"Critical {c+2} ")
+"""
+
